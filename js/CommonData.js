fd72acb23bfec6977432929e82114462f124f885
Carlos <carlos.horro@gmail.com>
2015-10-13 10:04:24 +0100

    Added many comments, removed duplicated resource types and unnecessary functions

--- js/CommonData.js
+++ js/CommonData.js
@@ -1,3 +1,6 @@
+/**
+ *          Auxiliar function to allow inheritance in a easy way.
+ */
 function inheritPrototype(childObject, parentObject) {
             // We copy the properties and methods from the parentObject onto the childObject
             // So the copyOfParent object now has everything the parentObject has
@@ -12,12 +15,18 @@ function inheritPrototype(childObject, parentObject) {
 
 
 
-
+/**
+ *          CommonData constructor
+ *          jsonData {Object} JSON data structure with the original data retrieved by our data server.
+ *
+ */
 function CommonData (jsonData) {
             this.jsonData = jsonData;
 };
 
-
+/**
+ *          Common parent class that should be inherited by all specific classes to be managed on this component.
+ */
 CommonData.prototype = {
             constructor: CommonData,
             SOURCE_FIELD                : "source",
@@ -27,7 +36,7 @@ CommonData.prototype = {
             DESCRIPTION_FIELD           : "description",
             LINK_FIELD                  : "link",
             
-            // retrieves the proper class based on the resource type
+            // retrieves the proper class name based on the real resource type
             mappingResourceTypeClasses : {
                         'Tool'                  :'tools',
                         'Workflow'              :'workflow',
@@ -36,46 +45,102 @@ CommonData.prototype = {
                         'Event'                 :'events'
             },
      
-            
+            /**         UTILITY FUNCTIONS TO GET FIELD'S VALUE                    */
+     
+            /**
+             *          Auxiliar function to get easily any kind of data present in the internal
+             *          data structure of this entity.
+             *          fieldName {String} - Name of the field to be retrieved.
+             *          
+             */
             getParameterisedValue : function(fieldName){
                         if (this.jsonData !== undefined && this.jsonData !== null){
                             return this.jsonData[fieldName];   
                         }else return null;
             },
+            
             // mandatory fields
             getSourceValue : function(){
                         return this.getParameterisedValue(this.SOURCE_FIELD);      
             },
+            /**
+             *          Returns all resource types present in this entity.
+             *          {Array} - Array of strings with resource typers related with this entity.
+             */
             getResourceTypeValues : function(){
                         return this.getParameterisedValue(this.RESOURCE_TYPE_FIELD);      
             },
+            /**
+             *          Sometimes can be duplicate resource types.
+             *          This function only returns unique resource types.
+             *          {Array} - Array of strings with unique resource typers related with this entity.
+             */
+            getUniqueResourceTypeValues : function(){
+                        var resourceTypes = this.getResourceTypeValues();
+                        var uniqueResourceTypes = [];
+                        for(var i=0;i<resourceTypes.length;i++){
+                                    console.log(i);
+                                    if (! (uniqueResourceTypes.indexOf(resourceTypes[i]) > -1)){
+                                                uniqueResourceTypes.push(resourceTypes[i]);   
+                                    }
+                        }
+                        return uniqueResourceTypes;
+            },
+            /**
+             *          Returns the title of this entity.
+             *          {String} - Title of this entity.
+             */
             getTitleValue : function(){
                         return this.getParameterisedValue(this.TITLE_FIELD);      
             },
+            /**
+             *          Returns all topic of this entity.
+             *          {Array} - Topics related with this entity.
+             */
             getTopicValue : function(){
                         return this.getParameterisedValue(this.TOPIC_FIELD);      
             },
             
             // optional fields
+            /**
+             *          Returns the description associated with this entity (if exists).
+             *          {String} - Textual description.
+             */
             getDescriptionValue : function(){
                         return this.getParameterisedValue(this.DESCRIPTION_FIELD);      
             },
+            /**
+             *          Returns the URL to access to the original source of this entity (if exists).
+             *          {String} - Source's URL.
+             */
             getLinkValue : function(){
                         return this.getParameterisedValue(this.LINK_FIELD);      
             },
       
       
-            // FUNCTIONS
+            /**         STANDARD FUNCTIONS TO MANAGE HTML BEHAVIOUR OF THIS ENTITY     */
       
+            /**
+             *          Returns one kind of CommonData transformed into a HTML component in a way that
+             *          depends on what kind of style you want it will be drawn.
+             *          displayStyle {String} - One drawing style. Currently ContextDataList.COMMON_STYLE or ContextDataList.FULL_STYLE.
+             *          {Object} - Array with HTML structured converted from this entity's original JSON status.
+             */
             getDrawableObject : function(displayStyle){
                         if (displayStyle == ContextDataList.COMMON_STYLE){
                                     return this.getCommonDrawableObject();
                         }else if (displayStyle == ContextDataList.FULL_STYLE){
+                                    // To implement this properly we will have to pass some kind of parameter or
+                                    // to call a different function.
                                     return this.getCommonDrawableObject();
                         }else return null;
             },
+            
+            /**
+             *          Returns one standard way of representing any CommonData transformed into a HTML component.
+             *          {Object} - Array with HTML structured converted from this entity's original JSON status.
+             */
             getCommonDrawableObject : function(){
-                        //var listElement = document.createElement('li');
                         var title = this.getLabelTitle();
                         var topics = this.getLabelTopics();
                         var resourceTypes = this.getImageResourceTypes();
@@ -101,35 +166,66 @@ CommonData.prototype = {
                         return mainContainer;
             },
       
-      
-      
-      
+            /**
+             *          Returns one standard way of representing 'title' data transformed into a HTML component.
+             *          {HTML Object} - ANCHOR element with 'title' information linking to the original source.
+             */
             getLabelTitle: function(){
                         var element = document.createElement('a');
+                        element.classList.add("context_data_title");
                         element.setAttribute('href',this.getLinkValue());
                         element.innerHTML = this.getTitleValue();
+                        var description = this.getDescriptionValue();
+                        if (description != undefined && description != null) {
+                                    element.title = description;
+                        }
                         element.setAttribute('target','_blank');
                         return element;
             },
             
+            /**
+             *          Returns one standard way of representing 'topics' data transformed into a HTML component.
+             *          {HTML Object} - DIV element with all 'topics' information related to this entity.
+             */
             getLabelTopics: function(){
                         var element = document.createElement('div');
-                        element.innerHTML = this.getTopicValue();
-                        return element;
-            },
-            getLabelResourceTypes: function(){
-                        var element = document.createElement('span');
-                        element.innerHTML = this.getResourceTypeValues();
+                        element.classList.add("context_data_topics");
+                        var rawTopicValue = this.getTopicValue();
+                        var finalString = '';
+                        for(var i=0;i<rawTopicValue.length;i++){
+                                    finalString = finalString + rawTopicValue[i];
+                                    if ((i+1) < rawTopicValue.length) {
+					finalString += ', ';
+                                    }
+                        }           
+                        element.innerHTML = finalString; 
                         return element;
             },
             
+            /**
+             *          Returns a standard textual way of representing 'resource type' data transformed into a HTML component.
+             *          {HTML Object} - SPAN element with all 'resource type' information related to this entity.
+             */
+            getLabelResourceTypes: function(){
+                        var element = document.createElement('span');
+                        element.innerHTML = this.getUniqueResourceTypeValues();
+                        return element;
+            },
+            
+            /**
+             *          Returns a standard way (as a set of images) of representing 'resource type'
+             *          data transformed into a HTML component.
+             *          {HTML Object} - SPAN element with all 'resource type' information related to this entity
+             *          represented as set of images.
+             */
             getImageResourceTypes: function(){
                         var container = document.createElement('span');
                         
-                        var resourceTypes = this.getResourceTypeValues();
+                        var resourceTypes = this.getUniqueResourceTypeValues();
                         for(var i=0;i<resourceTypes.length;i++){
                                     var resource_type = resourceTypes[i];
                                     var element = document.createElement('span');
+                                    element.title = resource_type;
                                     // flat gray style
                                     element.classList.add('flat_resource_type');
                                     element.classList.add('gray');
@@ -142,95 +238,8 @@ CommonData.prototype = {
                         return container;
             },
 
-            /**
-             * Private function to get the label for the title of a single citation
-             * 
-             *  @param resultElement single citation data in a JSON format
-             *  @param fontsize font size of the div containing the title
-             *  @param numCurrentRows number of current rows containing the citation data
-             *  @param fontfamily font-family of the div containing the title
-             */
-            getLabelTitleAnt: function(resultElement, fontsize, numCurrentRows, fontfamily){
-		var labelTitle = resultElement.title;
-		
-		if (this.opt.displayStyle == CitationList.COMPACT_STYLE){
-			var tempWidth = this.textWidth(jQuery('<span style="font-family:'+fontfamily+';font-size:'+fontsize+'px;">' + labelTitle + '</span>'));
-			var titleRows = Math.ceil(tempWidth/this._innerWidth);
-			
-			if (titleRows > (this.opt.numRowCompact - numCurrentRows)){
-				var indexLastTitle = (labelTitle.length * (this.opt.numRowCompact - numCurrentRows)) /  titleRows;
-				indexLastTitle = indexLastTitle -3;
-				labelTitle = labelTitle.substring(0, indexLastTitle)
-				labelTitle += "...";
-			}
-		}
-		
-		return labelTitle;
-	},
 
-            /**
-	 * Private function to get the label for the authors of a single citation
-	 * 
-	 *  @param resultElement single citation data in a JSON format
-	 *  @param fontsize font size of the div containing the authors
-	 *  @param numCurrentRows number of current rows containing the citation data
-	 *  @param fontfamily font-family of the div containing the authors
-	 */
-	getAuthorsElement: function (resultElement, fontSize, numCurrentRows, fontfamily){
-		
-		var authors = resultElement.authorList.author;
-		
-		var authorsHtml="";
-		var labelAuth="";
-		var authorName="";
-		var labelAuthNew="";
-		var filter="";
-		var tempWidth=0;
-		var numAuthRows = 0;
-		for (i=0; i < authors.length; i++){
-			author = authors[i];
-			if (i >0){
-				authorsHtml += ", ";
-				labelAuth += ", ";
-			}
-			
-			if (author.fullName!=""){
-				authorName = author.fullName;
-			}else if (author.lastName!=""){
-				authorName = author.lastName;
-			}else if (author.collectiveName!=""){
-				authorName = author.collectiveName;
-			}else{
-				authorName = "";
-			}
-			
-			if (authorName!=undefined && authorName!=""){ 
-				labelAuthNew =  labelAuth + authorName;
-				filter = "AUTH:&quot;"+authorName+"&quot;";
-				authorsHtmlNew =authorsHtml + "<a class=\"epmc_citation_link\" href=\"http://europepmc.org/search?query="+filter+"&page=1\">"+authorName+"</a>";
-				
-				if (this.opt.displayStyle == CitationList.COMPACT_STYLE){
-					tempWidth = this.textWidth(jQuery('<span style="font-family:'+fontfamily+';font-size:'+fontSize+'px;">' + labelAuthNew  + '</span>'));
-					numAuthRows = Math.ceil(tempWidth/this._innerWidth);
-					if (numAuthRows > (this.opt.numRowCompact - numCurrentRows)){
-						numAuthRows = (this.opt.numRowCompact - numCurrentRows);
-						authorsHtml = authorsHtml + "...";
-						labelAuth = labelAuth + "...";
-						break;
-					}else{
-						labelAuth = labelAuthNew;
-						authorsHtml = authorsHtmlNew;
-					}
-				}else{
-					labelAuth = labelAuthNew;
-					authorsHtml = authorsHtmlNew;
-				}
-			}
-		}
-		
-		this._tempLabel = labelAuth;
-		return authorsHtml;
-	}
+           
 };
 
 
