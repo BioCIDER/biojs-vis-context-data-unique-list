e5cd77ee03178fa5b8cc9879689a5001cb9afa88
Carlos <carlos.horro@gmail.com>
2015-11-04 15:13:41 +0000

    Starting with BioJS component

---
+++ js/index.js
@@ -0,0 +1,7 @@
+
+module.exports.ButtonsManager = require("./ButtonsManager.js");
+module.exports.CommonData = require("./CommonData.js");
+module.exports.ContextDataList = require("./ContextDataList.js");
+module.exports.DataManager = require("./DataManager.js");
+module.exports.PageManager = require("./PageManager.js");
+module.exports.BioCider = require("./BioCider.js");
---
+++ snippets/biojs_example.js
@@ -0,0 +1,9 @@
+
+
+var app = require("biojs-vis-context-data-unique-list");
+
+
+var bioCiderInstance = new app.BioCider('global_context_container',{},{},{});
+
+bioCiderInstance.draw();
+
