224b7fc2a71cec49f4d7cac1510976b953d82b66
Carlos <carlos.horro@gmail.com>
2015-12-02 15:22:06 +0000

    bug fixes to index.js

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 
-module.exports.constants = require("../js/constants.js");
-module.exports.ButtonsManager = require("../js/ButtonsManager.js");
-module.exports.CommonData = require("../js/CommonData.js");
-module.exports.ElixirEventData = require("../js/ElixirEventData.js");
-module.exports.ElixirRegistryData = require("../js/ElixirRegistryData.js");
-module.exports.ElixirTrainingData = require("../js/ElixirTrainingData.js");
-module.exports.ContextDataList = require("../js/ContextDataList.js");
-module.exports.DataManager = require("../js/DataManager.js");
-module.exports.PageManager = require("../js/PageManager.js");
-module.exports.BioCider = require("../js/BioCider.js");
+module.exports.constants = require("./constants.js");
+module.exports.ButtonsManager = require("./ButtonsManager.js");
+module.exports.CommonData = require("./CommonData.js");
+module.exports.ElixirEventData = require("./ElixirEventData.js");
+module.exports.ElixirRegistryData = require("./ElixirRegistryData.js");
+module.exports.ElixirTrainingData = require("./ElixirTrainingData.js");
+module.exports.ContextDataList = require("./ContextDataList.js");
+module.exports.DataManager = require("./DataManager.js");
+module.exports.PageManager = require("./PageManager.js");
+module.exports.BioCider = require("./BioCider.js");
